8c2f038838753047366171b41b5703d2041cbd95
plzgimmeaname <n00bsreekar@gmail.com>
2025-09-29 17:14:25 -0400

    Added voltage setting and basic configuration Changed to SparkMax from PWMSparkMax

--- src/main/java/frc/robot/Subsystems/OuttakeTest.java
+++ src/main/java/frc/robot/Subsystems/OuttakeTest.java
@@ -6,9 +6,11 @@ package frc.robot.Subsystems;
 
 import java.util.function.DoubleSupplier;
 import com.revrobotics.spark.SparkBase.PersistMode; 
-import com.revrobotics.spark.SparkBase.ResetMode; 
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -22,7 +24,20 @@ import edu.wpi.first.wpilibj2.command.Commands;
 public class OuttakeTest extends SubsystemBase {
   public static final int SPARKMAX_OUTTAKE_ID = 1;
 
-  PWMSparkMax outtakeMotor = new PWMSparkMax(SPARKMAX_OUTTAKE_ID);
+  SparkMax outtakeMotor = new SparkMax(SPARKMAX_OUTTAKE_ID, MotorType.kBrushed);
+
+  private void setVoltages(double volt) {
+    outtakeMotor.setVoltage(volt);
+    System.out.println(volt);
+  }
+
+  public Command setVoltagesCommand(DoubleSupplier volt) {
+    return this.run(() -> this.setVoltages(volt.getAsDouble()));
+  }
+
+  SparkMaxConfig config = new SparkMaxConfig();
+
+
   public OuttakeTest() {}
 
   @Override
