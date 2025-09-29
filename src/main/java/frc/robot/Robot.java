e977a635538e6326cca419d7f1e52189a9909fc7
John Steele <hcps-steelejc@henricostudents.org>
2025-09-28 21:28:07 -0400

    First Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.util.sendable.SendableRegistry;
+//import edu.wpi.first.wpilibj.PowerDistribution;
+//import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Subsystems.DrivetrainSubsystem;
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the manifest file in the resource directory.
+ */
+public class Robot extends TimedRobot {
+  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
+  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
+  private final DifferentialDrive m_robotDrive =
+      new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
+  private final XboxController m_controller = new XboxController(0);
+  private final Timer m_timer = new Timer();
+
+  CommandXboxController controller = new CommandXboxController(0);
+  
+  DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem(); 
+
+  private double modifyJoystick(double in) {
+    if (Math.abs(in) < 0.05) {
+      return 0; 
+    }
+    return in * in * Math.signum(in);
+  }
+
+  /** Called once at the beginning of the robot program. */
+  public Robot() {
+    SendableRegistry.addChild(m_robotDrive, m_leftDrive);
+    SendableRegistry.addChild(m_robotDrive, m_rightDrive);
+
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightDrive.setInverted(true);
+    System.out.println("Class: Robot Running.");
+
+    drivetrainSubsystem.setDefaultCommand(
+      drivetrainSubsystem.setVoltagesArcadeCommand(
+          () -> modifyJoystick(-controller.getLeftY()),
+          () -> modifyJoystick(-controller.getRightX())));
+  }
+
+
+
+  /** This function is run once each time the robot enters autonomous mode. */
+  @Override
+  public void autonomousInit() {
+    m_timer.restart();
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      // Drive forwards half speed, make sure to turn input squaring off
+      m_robotDrive.arcadeDrive(0.5, 0.0, false);
+    } else {
+      m_robotDrive.stopMotor(); // stop robot
+    }
+  }
+
+  /** This function is called once each time the robot enters teleoperated mode. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during teleoperated mode. */
+  @Override
+  public void teleopPeriodic() {
+    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+  }
+
+  /** This function is called once each time the robot enters test mode. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+}
---
+++ src/main/java/frc/robot/Subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix6.controls.VoltageOut;
+
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  public static final int LEFT_FRONT_TALON_ID = 1;
+  public static final int RIGHT_FRONT_TALON_ID = 2;
+  public static final int LEFT_BACK_TALON_ID = 3;
+  public static final int RIGHT_BACK_TALON_ID = 4; 
+
+
+  TalonSRX leftFrontTalon = new TalonSRX(LEFT_FRONT_TALON_ID);
+  TalonSRX rightFrontTalon = new TalonSRX(RIGHT_FRONT_TALON_ID);
+  TalonSRX leftBackTalon = new TalonSRX(LEFT_BACK_TALON_ID);
+  TalonSRX rightBackTalon = new TalonSRX(RIGHT_BACK_TALON_ID);
+
+  VoltageOut leftVoltage = new VoltageOut(0);
+  VoltageOut rightVoltage = new VoltageOut(0);
+
+
+  private void setVoltages(double left, double right) {
+    leftFrontTalon.set(ControlMode.PercentOutput, left);
+    rightFrontTalon.set(ControlMode.PercentOutput, right);
+    System.out.println(right);
+    System.out.println(left);
+  }
+
+  public Command setVoltagesCommand(DoubleSupplier left, DoubleSupplier right) {
+    return this.run(() -> this.setVoltages(left.getAsDouble(), right.getAsDouble()));
+  }
+
+  public Command setVoltagesArcadeCommand(DoubleSupplier drive, DoubleSupplier steer) {
+    System.out.println("Command: SetVoltagesArcadeCommand Running.");
+    return this.run(() -> {
+      System.out.println("Command: setVoltagesArcadeCommand 2 Running.");
+      var speeds = DifferentialDrive.arcadeDriveIK(drive.getAsDouble(), steer.getAsDouble(), false);
+      this.setVoltages(speeds.left * 12, speeds.right * 12);
+    });
+  }
+
+
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {
+      leftBackTalon.follow(leftFrontTalon);
+      rightBackTalon.follow(rightFrontTalon); 
+      rightFrontTalon.setInverted(true);
+      rightBackTalon.setInverted(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
