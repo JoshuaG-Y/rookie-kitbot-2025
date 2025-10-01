935c11c4848fa7ce52dcc699732e9b087355f50b
JoshuaG-Y <hcps-groveryja@henricostudents.org>
2025-10-01 15:53:57 -0400

    DrivetrainIO class and extension class

---
+++ src/main/java/frc/robot/Subsystems/Drive/DrivetrainIO.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.Drive;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Pose2d;
+
+public interface DrivetrainIO {
+    @AutoLog
+    public static class DrivetrainIOInputs {
+        public Pose2d robotPose = new Pose2d();
+        public double leftOutputVolts = 0.0;
+        public double rightOutputVolts = 0.0;
+
+        public double leftVelocityMetersPerSecond = 0.0;
+        public double rightVelocityMetersPerSecond = 0.0;
+
+        public double leftPositionMeters = 0.0;
+        public double rightPositionMeters = 0.0;
+
+        public double[] leftCurrentAmps = new double[0];
+        public double[] rightCurrentAmps = new double[0];
+    }
+
+    
+    public void updateInputs(DrivetrainIOInputs inputs);
+    
+    public void arcadeDrive(double left, double right);
+    
+} 
