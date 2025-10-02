45fa178b53189514a976bb2a6448cf9c79b8f33b
JoshuaG-Y <hcps-groveryja@henricostudents.org>
2025-10-02 17:28:28 -0400

    Remove unused imports

--- src/main/java/frc/robot/Subsystems/OuttakeTest.java
+++ src/main/java/frc/robot/Subsystems/OuttakeTest.java
@@ -4,23 +4,16 @@
 
 package frc.robot.Subsystems;
 
-import java.io.ObjectInputFilter.Config;
 import java.util.function.DoubleSupplier;
 import com.revrobotics.spark.SparkBase.PersistMode; 
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import com.ctre.phoenix6.controls.VoltageOut;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 public class OuttakeTest extends SubsystemBase {
   public static final int SPARKMAX_OUTTAKE_ID = 1;
